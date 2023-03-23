d13686d09d9af3abdfec99984cdba2745356b2ea
Максим <maksim@MacBook-Air-Exon.local>
2023-03-23 12:20:59 +0300

    flake8

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'infra_app'
@@ -7,5 +6,4 @@ app_name = 'infra_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
-
 ]
